2643d13844ae08e4c262d7d1e4063605d59d481e
ErikGa25 <jerik67@gmail.com>
2021-07-01 19:37:06 -0500

    funcion para validar inputs, se implemento sweetalert

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -7,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: UsuarioModel;
+  recordarme = false;
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+  }
 
+  onSubmit(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    })
+
+    Swal.showLoading()
+    
+    this.auth.nuevoUsuario(this.usuario).subscribe( resp => {
+      Swal.close()
+
+      if(this.recordarme) {
+        localStorage.setItem('email', this.usuario.email);
+      }
+
+      this.router.navigateByUrl('/home');
+    },(err) => {
+      console.log(err)
+      Swal.fire({
+        allowOutsideClick: true,
+        title: 'Error al autenticar',
+        icon: 'error',
+        text: err.error.error.message
+      })
+    });
+  }
 }
